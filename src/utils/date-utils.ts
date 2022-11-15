f17b5f114c766140ea0790b40c66dcfa50d20fd9
Jonathan Linowes <jonathan@parkerhill.com>
2022-11-15 14:23:46 -0500

    build fix

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -1,24 +1,26 @@
-import { format } from 'date-fns'
-import { isBlank } from './string-utils'
+import { format } from 'date-fns';
+import { isBlank } from './string-utils';
 
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
-export function formatDate(dateStr, abbreviated = false) {
-  if (isBlank(dateStr)) return ''
-  const date = new Date(dateStr)
+export function formatDate(dateStr: string, abbreviated = false): string {
+  if (isBlank(dateStr)) return '';
+  const date = new Date(dateStr);
   // if (abbreviated && date.getFullYear() == currentYear) {
   //   return format(date, 'd MMM')
   // } else {
-  return abbreviated ? format(date, 'd MMM yyyy') : format(date, 'MMMM d, yyyy')
+  return abbreviated
+    ? format(date, 'd MMM yyyy')
+    : format(date, 'MMMM d, yyyy');
   // }
 }
 
-export function formatDateTime(dateStr) {
-  if (isBlank(dateStr)) return ''
-  const date = new Date(dateStr)
-  return format(date, 'd MMM yyyy, HH:mm:ss')
+export function formatDateTime(dateStr: string): string {
+  if (isBlank(dateStr)) return '';
+  const date = new Date(dateStr);
+  return format(date, 'd MMM yyyy, HH:mm:ss');
 }
 
-export function formatForFilename(date) {
-  return format(date, 'yyyyMMddHHmmss')
+export function formatForFilename(date: Date): string {
+  return format(date, 'yyyyMMddHHmmss');
 }
--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,120 +1,120 @@
+//---------------
+
+// https://stackoverflow.com/a/1431113/505018
+// @ts-ignore
+export function replaceAt(str: string, index: number, replacement: string) {
+  return (
+    str.substring(0, index) +
+    replacement +
+    str.substring(index + replacement.length)
+  );
+}
+
 //
 // https://stackoverflow.com/questions/27440917/change-any-type-of-string-to-title-case-in-javascript
-export function phraseToTitleCase(s) {
+export function phraseToTitleCase(s: string): string {
   return (
     s
       // .replace(/([^A-Z])([A-Z])/g, '$1 $2') // split cameCase
       // .replace(/[_\-]+/g, ' ') // split snake_case and lisp-case
       // .toLowerCase()
       .replace(/(^\w|\b\w)/g, function (m) {
-        return m.toUpperCase()
+        return m.toUpperCase();
       }) // title case words
       .replace(/\s+/g, ' ') // collapse repeated whitespace
       .replace(/^\s+|\s+$/, '')
-  ) // remove leading/trailing whitespace
+  ); // remove leading/trailing whitespace
 }
 
-export function titleize(s) {
-  return phraseToTitleCase(s)
+export function titleize(s: string): string {
+  return phraseToTitleCase(s);
 }
 
-export function camelToTitleCase(s) {
+export function camelToTitleCase(s: string): string {
   return (
     s
       .replace(/([^A-Z])([A-Z])/g, '$1 $2') // split cameCase
       // .replace(/[_\-]+/g, ' ') // split snake_case and lisp-case
       // .toLowerCase()
       .replace(/(^\w|\b\w)/g, function (m) {
-        return m.toUpperCase()
+        return m.toUpperCase();
       }) // title case words
       .replace(/\s+/g, ' ') // collapse repeated whitespace
       .replace(/^\s+|\s+$/, '')
-  ) // remove leading/trailing whitespace
+  ); // remove leading/trailing whitespace
 }
 
-export function snakeToTitleCase(s) {
+export function snakeToTitleCase(s: string): string {
   return s
     .replace(/([^A-Z])([A-Z])/g, '$1 $2') // split cameCase
     .replace(/[_\-]+/g, ' ') // split snake_case and lisp-case
     .toLowerCase()
     .replace(/(^\w|\b\w)/g, function (m) {
-      return m.toUpperCase()
+      return m.toUpperCase();
     }) // title case words
     .replace(/\s+/g, ' ') // collapse repeated whitespace
-    .replace(/^\s+|\s+$/, '') // remove leading/trailing whitespace
+    .replace(/^\s+|\s+$/, ''); // remove leading/trailing whitespace
 }
 
-export function surnameCase(s) {
-  let name = phraseToTitleCase(s.trim().toLowerCase())
-  if (name.startsWith('Mc')) name = name.replaceAt(2, name[2].toUpperCase())
-  if (name.startsWith('Mac')) name = name.replaceAt(3, name[3].toUpperCase())
-  if (name.startsWith("O'")) name = name.replaceAt(2, name[2].toUpperCase())
+export function surnameCase(s: string): string {
+  let name = phraseToTitleCase(s.trim().toLowerCase());
+  if (name.startsWith('Mc')) name = replaceAt(name, 2, name[2].toUpperCase());
+  if (name.startsWith('Mac')) name = replaceAt(name, 3, name[3].toUpperCase());
+  if (name.startsWith("O'")) name = replaceAt(name, 2, name[2].toUpperCase());
   // todo if theres more pull these from a list
-  return name
+  return name;
 }
 
 //-----------
 
-export function slugify(name, id = 0) {
+export function slugify(name: string, id = 0): string {
   // https://gist.github.com/codeguy/6684588#gistcomment-3777802
-  let str = name ? name.replace(/^\s+|\s+$/g, '') : '' // trim
-  str = str.toLowerCase()
+  let str = name ? name.replace(/^\s+|\s+$/g, '') : ''; // trim
+  str = str.toLowerCase();
 
   // remove accents, swap ñ for n, etc
-  var from = 'àáãäâèéëêìíïîòóöôùúüûñç·/_,:;'
-  var to = 'aaaaaeeeeiiiioooouuuunc------'
+  var from = 'àáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  var to = 'aaaaaeeeeiiiioooouuuunc------';
 
   for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
   }
 
   str = str
     .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
     .replace(/\s+/g, '-') // collapse whitespace and replace by -
-    .replace(/-+/g, '-') // collapse dashes
+    .replace(/-+/g, '-'); // collapse dashes
 
   if (id != 0) {
-    str = `${id}-${str}`
+    str = `${id}-${str}`;
   }
-  return str
+  return str;
 }
 
 //---------------
 
 // https://stackoverflow.com/a/3261380/505018
 // for checking if a string is empty, null or undefined (doesnt check for whitespace) eg isEmpty(str)
-export function isEmpty(str) {
-  return !str || 0 === str.length
+export function isEmpty(str: string): boolean {
+  return !str || 0 === str.length;
 }
 
 // for checking if a string is empty, only whitespace, null or undefined eg isBlank(str)
-export function isBlank(str) {
-  return !str || /^\s*$/.test(str)
+export function isBlank(str: any): boolean {
+  return !str || /^\s*$/.test(str);
 }
 
 // for checking if a string is blank or contains only white-space: eg if ( str.isEmpty() )
 // @ts-ignore
 String.prototype.isEmpty = function () {
-  return this.length === 0 || !this.trim()
-}
+  return this.length === 0 || !this.trim();
+};
 
-export function isPresent(str) {
-  return !isBlank(str)
-}
-
-//---------------
-
-// https://stackoverflow.com/a/1431113/505018
-// @ts-ignore
-String.prototype.replaceAt = function (index, replacement) {
-  return (
-    this.substr(0, index) +
-    replacement +
-    this.substr(index + replacement.length)
-  )
+export function isPresent(str: any): boolean {
+  return !isBlank(str);
 }
 
 export function lastWord(fullname: string): string | undefined {
-  const names = fullname.split(' ')
-  return names[names.length - 1]
+  const names = fullname.split(' ');
+  return names[names.length - 1];
 }
